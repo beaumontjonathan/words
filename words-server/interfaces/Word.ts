2a6ae1fde94a2cda2c29d7b25e5d68a6fcd1fc5a
castlchef <beaumont_jonny@hotmail.co.uk>
2017-06-11 02:12:30 +0100

    Added Word interface to contain elements returned from database words table queries.

---
+++ words-server/interfaces/Word.ts
@@ -0,0 +1,13 @@
+/**
+ * <h1>Words</h1>
+ * Stores information about a word record from the database.
+ *
+ * @author  Jonathan Beaumont
+ * @version 1.0.0
+ * @since   2017-06-11
+ */
+export interface Word {
+  id: number;
+  word: string;
+}
+
